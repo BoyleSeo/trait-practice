085e2be4ca47b41e5f8511b3c1351859d7da4990
BoyleSeo <ozorion0@gmail.com>
2023-06-26 02:57:47 +0900

    Change conditional compilation config

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,15 @@
-mod js_map;
-mod other;
-mod terminal;
-
 #[cfg(feature = "js_map")]
-use js_map::*;
-#[cfg(not(any(feature = "js_map", feature = "terminal_api")))]
-use other::*;
-#[cfg(feature = "terminal_api")]
-use terminal::*;
+#[path = "js_map/mod.rs"]
+mod feature;
 
+#[cfg(not(any(feature = "js_map", feature = "terminal_api")))]
+#[path = "other.rs"]
+mod feature;
+#[cfg(feature = "terminal_api")]
+#[path = "terminal.rs"]
+mod feature;
+
+use feature::*;
 fn main() {
     println!("\n[VERSION 1]");
     version_1::test();
