a92339809744e8fd941445de4c5479c5e5e2ac98
BoyleSeo <ozorion@naver.com>
2023-07-20 18:54:01 +0900

    Add a macro practice idea

--- src/main.rs
+++ src/main.rs
@@ -15,4 +15,25 @@ fn main() {
     version_1::test();
     println!("\n[VERSION 2]");
     version_2::test();
+    dbg!(Enum::try_from("crane"));
+}
+#[derive(Debug)]
+enum Enum {
+    Asimo,
+    Balaclava,
+    Crane,
+    DdongJengE,
+}
+//이런건 매크로로 만드는 실습 하면 좋을 듯. derive 가능하게
+impl TryFrom<&str> for Enum {
+    type Error = ();
+    fn try_from(value: &str) -> Result<Self, Self::Error> {
+        match value {
+            "asimo" => Ok(Enum::Asimo),
+            "balaclava" => Ok(Enum::Balaclava),
+            "ddongJengE" => Ok(Enum::DdongJengE),
+            "crane" => Ok(Enum::Crane),
+            _ => Err(()),
+        }
+    }
 }
